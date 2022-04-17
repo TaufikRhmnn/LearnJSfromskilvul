d411c6f5f7bc7e4067d310799e321209264e504c
TaufikRhmnn <rohmannamhor44@gmail.com>
2022-04-17 08:08:20 +0700

    first commit

---
+++ javascript.js
@@ -0,0 +1,69 @@
+console.log("hello world javascrpt")
+// Deklarasi Variable
+
+var nama = "Taufik"
+var harga = 2000
+var apaSudahMenikah = false
+
+
+
+console.log(nama)
+console.log(harga)
+console.log(apaSudahMenikah)
+
+
+
+// Operator Aritmatika
+
+var nilai1 = 10 / 2
+
+console.log(nilai1)
+
+
+// Operator increment & decrementt
+console.log('operator increment dan decrement')
+var nilai2 = 4
+// nilai2++
+nilai2--
+
+console.log(nilai2)
+
+
+//Operator Assignment
+
+console.log('operator assignment')
+
+var nilai3 = 10
+nilai3 += 20
+
+console.log(nilai3)
+
+
+// Operator string dan number
+
+console.log ('operator string dan number')
+
+var namaDepan=' Taufik '
+var namaBelakang='rohman'
+
+console.log(namaDepan + namaBelakang)
+console.log(namaDepan + namaBelakang)
+
+var angka1=1 
+console.log(angka1 + namaDepan)
+
+
+// Operator perbandingan != !== <= >=
+
+console.log('operator perbandingan')
+
+console.log(2!==2)
+console.log(9!=1)
+
+
+// Operator logika && || !
+console.log('operator logika')
+var sudahMenikah = true
+var sudahPunyaAnak=false
+
+console.log(sudahMenikah || sudahPunyaAnak )
